0c9650cc5a7d959fcecb76db38b45c51b5d96ebd
hyunsung0815 <iter000513@gmail.com>
2025-05-10 15:58:14 +0900

    커밋 메시지

--- mysite/pybo/urls.py
+++ mysite/pybo/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('page/', views.page, name='page'),
+    path('case1/', views.case1_page, name='case1_page'),
 ]
--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'main_page.html')
+
+def page(request):
+    return render(request, 'page.html')
+
+def case1_page(request):
+    return render(request, 'case1_page.html')
